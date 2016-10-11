b20f93eb992df584760f388c976dfc164f555658
Syntaxious <SynPlaysMC@gmail.com>
2016-10-11 19:35:35 +0100

    Update Main.php

--- src/TurfWars/Main.php
+++ src/TurfWars/Main.php
@@ -1,8 +1,24 @@
 <?php
 
+namespace TurfWars;
 
-  class Main extends PluginBase {
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener {
     
-  }
+    public $blocks;
+    public $commands;
+    
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(new EventsListener ($this), $this);
+        
+    }
+}
 
-?>
